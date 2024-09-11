f6cba40ff34d31a25d05c855f7943fa43943448e
LuizPauloAraujo <luiz.paulo.araujo@hotmail.com>
2024-09-10 23:05:57 -0300

    conclusao

---
+++ dia8/dia8.py
@@ -0,0 +1,7 @@
+def celsius_fahrenheit():
+   c = float(input('Digite a temperatura em °c'))
+   f = float((9 * c) / 5) + 32
+
+   return print('A temperatura em fahrenheit: {0}°F'.format(f))
+
+celsius_fahrenheit()
